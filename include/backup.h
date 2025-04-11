a87927dfb1881e9dcb83fb704fbb78be397b5cbe
Shu Hui Chen <shu@aerospike.com>
2025-04-11 15:14:48 -0700

    TOOLS-3037 - Add partition-list option.

--- include/backup.h
+++ include/backup.h
@@ -1,7 +1,7 @@
 /*
  * Aerospike Backup
  *
- * Copyright (c) 2008-2024 Aerospike, Inc. All rights reserved.
+ * Copyright (c) 2008-2025 Aerospike, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -36,6 +36,8 @@
 #define MAX_PARALLEL 100                            ///< Allow up to this many nodes to be backed up
                                                     ///  in parallel.
 
+#define MAX_PARTITIONS 4096
+
 ///
 /// The interface exposed by the backup file format encoder.
 ///
@@ -50,7 +52,7 @@ typedef struct {
 	///
 	/// @result         `true`, if successful.
 	///
-	bool (*put_record)(uint64_t *bytes, FILE *fd, bool compact, const as_record *rec);
+	bool (*put_record)(uint64_t* bytes, FILE* fd, bool compact, const as_record* rec);
 } backup_encoder;
 
 typedef struct cdt_stats_s {
@@ -73,32 +75,32 @@ typedef struct cdt_stats_s {
 /// The global backup configuration and stats shared by all backup threads and the counter thread.
 ///
 typedef struct {
-	char *host;
+	char* host;
 	bool use_services_alternate;
 	int32_t port;
-	char *user;
-	char *password;
+	char* user;
+	char* password;
 	bool remove_files;
-	char *bin_list;
-	char *node_list;
+	char* bin_list;
+	char* node_list;
 
 	as_config_tls tls;
 
-	aerospike *as;                      ///< The Aerospike client to be used for the node scans.
-	as_policy_scan *policy;             ///< The scan policy to be used for the node scans.
-	as_scan *scan;                      ///< The scan configuration to be used for the node scans.
-	char *directory;                    ///< The backup directory. `NULL`, when backing up to a
+	aerospike* as;                      ///< The Aerospike client to be used for the node scans.
+	as_policy_scan* policy;             ///< The scan policy to be used for the node scans.
+	as_scan* scan;                      ///< The scan configuration to be used for the node scans.
+	char* directory;                    ///< The backup directory. `NULL`, when backing up to a
 	                                    ///  single file.
-	char *output_file;                  ///< The backup file. `NULL`, when backing up to a
+	char* output_file;                  ///< The backup file. `NULL`, when backing up to a
 	                                    ///  directory.
 	bool compact;                       ///< Disables base-64 encoding for BLOB bin values.
 	int32_t parallel;                   ///< The maximal number of cluster nodes scanned in
 	                                    ///  parallel.
-	char *machine;                      ///< The path for the machine-readable output.
+	char* machine;                      ///< The path for the machine-readable output.
 	uint64_t bandwidth;                 ///< The B/s cap for throttling.
 	uint64_t file_limit;                ///< Start a new backup file when the current backup file
 	                                    ///  crosses this size.
-	backup_encoder *encoder;            ///< The file format encoder to be used for writing data to
+	backup_encoder* encoder;            ///< The file format encoder to be used for writing data to
 	                                    ///  a backup file.
 	uint64_t rec_count_estimate;        ///< The number of objects to be backed up. This can change
 	                                    ///  during the backup, so it's just treated as an estimate.
@@ -109,7 +111,12 @@ typedef struct {
 	volatile uint64_t byte_count_limit; ///< The current limit for byte_count_total for throttling.
 	                                    ///  This is periodically increased by the counter thread to
 	                                    ///  raise the limit according to the bandwidth limit.
-	char *auth_mode;					///< Authentication mode
+	char* auth_mode;					///< Authentication mode
+
+	// String containing partition range
+	char* partition_str;
+	//as_partition_filter p_filter;
+	as_vector filters_v;
 
 	bool cdt_fix;
 	bool check_map_keys;
@@ -122,13 +129,16 @@ typedef struct {
 /// The per-node information pushed to the job queue and picked up by the backup threads.
 ///
 typedef struct {
-	backup_config *conf;                ///< The global backup configuration and stats.
+	backup_config* conf;                ///< The global backup configuration and stats.
 	char node_name[AS_NODE_NAME_SIZE];  ///< The node ID of the cluster node to be backed up.
-	FILE *shared_fd;                    ///< When backing up to a single file, the file descriptor
+	FILE* shared_fd;                    ///< When backing up to a single file, the file descriptor
 	                                    ///  of that file.
 	uint64_t bytes;                     ///< When backing up to a single file, the number of bytes
 	                                    ///  that were written when open_file() created that file
 	                                    ///  (version header, meta data).
+
+	as_partition_filter filter; // partition ranges/digest to be backed up
+	bool use_partition_filter;
 } backup_thread_args;
 
 ///
@@ -139,11 +149,11 @@ typedef struct {
 	char node_name[AS_NODE_NAME_SIZE];  ///< The node ID of the currently processed cluster node.
 	backup_config *conf;                ///< The global backup configuration and stats. Copied from
 	                                    ///  backup_thread_args.conf.
-	FILE *shared_fd;                    ///< When backing up to a single file, the file descriptor
+	FILE* shared_fd;                    ///< When backing up to a single file, the file descriptor
 	                                    ///  of that file. Copied from backup_thread_args.shared_fd.
-	FILE *fd;                           ///< The file descriptor of the current backup file for the
+	FILE* fd;                           ///< The file descriptor of the current backup file for the
 	                                    ///  currently processed cluster node.
-	void *fd_buf;                       ///< When backing up to a directory, the I/O buffer
+	void* fd_buf;                       ///< When backing up to a directory, the I/O buffer
 	                                    ///  associated with the current backup file descriptor.
 	uint32_t file_count;                ///< When backing up to a directory, counts the number of
 	                                    ///  backup files created for the currently processed
