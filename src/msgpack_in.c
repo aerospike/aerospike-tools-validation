18f0fd28df6c29e51bc1d608b8d03aef50b33273
mcoberly2 <mcoberly@aerospike.com>
2026-09-11 03:37:34 -0700

    MCC-TOOLS-3669 Update copyright year to 2026.

--- src/msgpack_in.c
+++ src/msgpack_in.c
@@ -1,7 +1,7 @@
 /*
  * msgpack_in.c
  *
- * Copyright (C) 2019-2024 Aerospike, Inc.
+ * Copyright (C) 2019-2026 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
